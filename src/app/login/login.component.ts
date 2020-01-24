860d036499fe7672c679c1015d611771061a382d
Jeff Lapp <lappjeff20@gmail.com>
2020-01-23 19:09:30 -0500

    Started work on dynamic blur functionality

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
+  isBlurred: boolean = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  setIsBlurred() {
+    this.isBlurred = !this.isBlurred;
   }
 
+  constructor() {}
+
+  ngOnInit() {}
 }
